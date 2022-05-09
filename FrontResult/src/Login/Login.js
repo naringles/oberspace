653c71c8b660428dc81306a80a69331dee6abb9a
minsun1103 <kkang5813@naver.com>
2022-05-10 03:16:52 +0900

    12

--- FrontResult/src/Login/Login.js
+++ FrontResult/src/Login/Login.js
@@ -7,18 +7,20 @@ render() {
   return (
     <div className="App">
       <div className="obserSpace">
-      <img className='L'
+      <img className='L1'
           src={require('../LogoImage.jpeg')}
       />
       </div>
 
       <div>
-        <div className="iconId"><AiOutlineUser size="40" color="gray"/></div>
-        <input type="text" className="loginId" placeholder='  아이디'></input>
+        <div className="layoutId">
+          <AiOutlineUser size="40" color='gray' className='IconId'/></div>
+        <input type="text" className="loginId" placeholder="  아이디"></input>
       </div>
 
       <div>
-        <div className="iconPw"><AiFillLock size="40" color="gray"/></div>
+        <div className="layoutPw">
+          <AiFillLock size="40" color="gray" className='IconPw'/></div>
         <input type="password" className="loginPw" placeholder='  비밀번호'></input>
       </div>
 
