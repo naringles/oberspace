0b5788fa0f1eb7e2ab107c2563e12481e267d507
naringles <kkang5813@naver.com>
2022-05-22 12:36:06 +0900

    g

---
+++ src/data.js
@@ -0,0 +1,17 @@
+export const data1 = [
+  {
+    id: 0,
+    value: ["google Drive", "Progress"],
+    allCheck: false,
+  },
+  {
+    id: 1,
+    value: ["One Drive", "OneDriveProgress"],
+    allCheck: false,
+  },
+  {
+    id: 2,
+    value: ["Mega drive", "MegaDriveProgress"],
+    allCheck: false,
+  },
+];
---
+++ src/user/index.js
@@ -0,0 +1,100 @@
+import { AiOutlineCloud } from "react-icons/ai";
+
+import styled from "styled-components";
+import Swal from "sweetalert2";
+import { useEffect, useState } from "react";
+import { data1 } from "../data";
+
+export default function User() {
+  const [driveMap, setDriveMap] = useState(
+    () => JSON.parse(window.localStorage.getItem("data")) || data1
+  );
+
+  useEffect(() => {
+    window.localStorage.setItem("data", JSON.stringify(driveMap));
+  }, [driveMap]);
+
+  const deleteDrive = (str, i) => {
+    const swalWithBootstrapButtons = Swal.mixin({
+      customClass: {
+        confirmButton: "btn btn-success",
+      },
+      buttonsStyling: false,
+    });
+
+    swalWithBootstrapButtons
+      .fire({
+        title: `${str}를 삭제하시겠습니까?`,
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonText: "Yes",
+        cancelButtonText: "No",
+        reverseButtons: true,
+      })
+      .then((result) => {
+        if (result.isConfirmed) {
+          swalWithBootstrapButtons.fire(
+            `${str}가 삭제되었습니다`,
+            "",
+            "success"
+          );
+
+          setDriveMap(
+            driveMap.map((data) => ({
+              ...data,
+              value: data.id !== i ? data.value : "",
+            }))
+          );
+          console.log(driveMap);
+        }
+      });
+  };
+
+  return (
+    <>
+      {driveMap.map((item, i) => (
+        <>
+          {item.value !== "" ? (
+            <div className="CloudBox">
+              <div className="CloudBoxUp">
+                <div className="">
+                  <AiOutlineCloud size="40" />
+                </div>
+                <div className="CloudBoxUpText">
+                  <div className="GoogleDriveText">
+                    {item.value[0]}
+                    <span
+                      className="OneDriveDel"
+                      onClick={() => deleteDrive(item.value[0], i)}
+                    >
+                      삭제
+                    </span>
+                  </div>
+                  <span>string</span>
+                </div>
+              </div>
+
+              <div className="ProgressDiv">
+                <progress
+                  value="50"
+                  max="80"
+                  className={
+                    item.value[1] === "Progress"
+                      ? "Progress"
+                      : item.value[1] === "OneDriveProgress"
+                      ? "OneDriveProgress"
+                      : "MegaDriveProgress"
+                  }
+                />
+              </div>
+
+              <span className="StorageVolume">50GB / 80GB</span>
+            </div>
+          ) : (
+            <></>
+          )}
+        </>
+      ))}
+    </>
+  );
+}
