1640f434ca0198f4394ce86af1a1123d786f4a8e
naringles <kkang5813@naver.com>
2022-06-06 02:54:15 +0900

    uploading

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -2,8 +2,7 @@ import React, {useState,Component} from 'react';
 import './Login.css';
 import { AiFillLock,AiOutlineUser} from "react-icons/ai";
 import {Route, Link} from 'react-router-dom';
-
-//+회원가입 밑줄 없애기
+import swal from "sweetalert"
 
 class Login extends Component {
 render() {
@@ -26,11 +25,21 @@ render() {
           <AiFillLock size="40" color="gray" className='IconPw'/></div>
         <input type="password" className="loginPw" placeholder='  비밀번호'></input>
       </div>
-
-      <input type="submit" className="login" value="로그인"></input>
-
+      
+      
+      <input type="submit" className="login" value="로그인" 
+      onClick = {()=>{
+        swal("로그인 성공", "환영합니다", "success")
+            .then(()=>{
+              window.location.replace('/');
+            })
+      }}></input>
+      
       <div className="join1">
-           <Link to = "/SignUp">SIGN UP</Link>
+           <Link className="a" to = "/SignUp" >
+             <a className='App-link'>+ 회원가입</a>
+          </Link>
+        
       </div>
      </div>
   );
